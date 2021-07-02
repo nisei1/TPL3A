9a2e22b8b47524451e42f5b74f56303cdccfce8b
nisei1 <43508107+nisei1@users.noreply.github.com>
2021-07-02 14:32:13 +0900

    2021-07-02-TPL3A-Fri-1
    グローバル変数の命名規則を"g_***"に変更
    試行回数とCSVファイル出力を追加

--- chaosSearch/test2/chaosSearch2.cpp
+++ chaosSearch/test2/chaosSearch2.cpp
@@ -9,15 +9,17 @@
 
 /***定数の宣言***/
 //ファイル入出力用の定数宣言
-#define IN_NAME "../../TSP/rbg443a.txt" //読み込みたいTSP
-#define OUT_NAME "out.txt"				//書き出したいファイル名
+#define IN_TSP_NAME "../../TSP/p43a.txt" //読み込みたいTSP
+#define OUT_DEBUG_NAME "outDebug.txt"	 //書き出したいファイル名
+#define CSV_NAME "outCost.csv"			 //書き出したいcsvファイル名
 //TSP用定数宣言
-#define CITY_NUM 443 //TSPの都市数
+#define CITY_NUM 43 //TSPの都市数
 //ランダムに2-optする時用の定数宣言
 #define ENABLE_TWO_OPT_RANDOM false //ランダム2-opt 有効= true ,無効 = false
 #define TWO_OPT_TIMES 10			//2optで何回最小値を出すか,最小値を出すまでループで減らない
 //カオスサーチで使う定数の宣言
-#define T_TIMES 10				 //時刻tがどこまで増やすのか適当に決めてよい
+#define ATTEMPT_TIMES 3			 //試行回数-適当に決めてよい
+#define T_TIMES 100				 //時刻tがどこまで増やすのか適当に決めてよい
 #define ENABLE_CHAOS_SEARCH true //カオスサーチするか 有効= true ,無効 = false
 #define ALPHA 1.0
 #define BETA 75.0
@@ -49,27 +51,27 @@ struct ChaosNN
 
 /***グローバル変数の宣言***/
 //2-optの時にも利用
-std::vector<std::vector<int>> edge(CITY_NUM, std::vector<int>(CITY_NUM)); //TSPを表す2次元vector、例) 1 -> 2 のコストを要素へ記録 edge[0][1] = 1から2へ行くためのコスト格納
-std::vector<int> city;													  //巡回路用vector、edgeの要素番号へ入れるのに使用
-std::ofstream out(OUT_NAME);											  //ファイル出力用変数
-std::ifstream in(IN_NAME);												  //ファイル入力用変数
+std::vector<std::vector<int>> g_Edge(CITY_NUM, std::vector<int>(CITY_NUM)); //TSPを表す2次元vector、例) 1 -> 2 のコストを要素へ記録 g_Edge[0][1] = 1から2へ行くためのコスト格納
+std::vector<int> g_City;													//巡回路用vector、g_Edgeの要素番号へ入れるのに使用
+std::ofstream g_OutDebug(OUT_DEBUG_NAME);									//デバッグ出力用変数
+std::ofstream g_OutCSV(CSV_NAME);											//csv出力用変数
+std::ifstream g_TSP_In(IN_TSP_NAME);										//ファイル入力用変数
 //ランダム用儀式
-time_t t = time(NULL);
-std::random_device seed_gen;
-std::mt19937 engine(seed_gen() * t);
+time_t g_Time = time(NULL);
+std::random_device g_Seed_Gen;
+std::mt19937 g_Engine(g_Seed_Gen() * g_Time);
 //カオスサーチに利用
-std::vector<ChaosNN> cnn(T_TIMES); //カオスサーチ用のvector<ChaosNN>
-// MaxDelta_I_J maxIJ;			  //Δijが最大にコストを下げた時のijを格納
+std::vector<ChaosNN> g_Cnn(T_TIMES); //カオスサーチ用のvector<ChaosNN>
 
 /***テンプレート関数の宣言***/
-void inputTSP(void);						  //TSPをedgeへ格納する関数
-void makeFirstTour(void);					  //初回巡回路をランダムに作成する関数
+inline void inputTSP(void);					  //TSPをg_Edgeへ格納する関数
+inline void makeFirstTour(void);			  //初回巡回路をランダムに作成する関数
 inline int calcDistance(void);				  //巡回路の総コスト計算関数(戻り値:(int)巡回路の総コスト)
 inline void twoOptRandom(void);				  //ランダムな2点を選んで2-opt交換する関数
 inline bool twoOptPermission(int p1, int p2); //ランダム2-opt可能な2点かどうか判定(引数:都市1,都市2)(戻り値:true or false)
 inline void twoOptSwap(int p1, int p2);		  //ランダム2-opt交換実行関数(引数:都市1,都市2)
 inline bool swapPermission(int p1, int p2);	  //2-opt可能な2点かどうか判定(引数:都市1,都市2)(戻り値:true or false)
-void initializeChaosNN(void);				  //時刻tの時の初期値
+inline void initializeChaosNN(void);		  //時刻tの時の初期値
 inline double sigmoid(double x);			  //シグモイド関数
 inline double calcZai(int t, int i);		  //(3)式関数
 inline double calcEta(int t, int i);		  //(4)式関数
@@ -84,102 +86,106 @@ int main(int argc, char const *argv[])
 
 	if (ENABLE_TWO_OPT_RANDOM)
 	{
-		out << "EnableTwoOptRandom" << std::endl;
+		g_OutDebug << "EnableTwoOptRandom" << std::endl;
 		makeFirstTour();
 		//最適化前に巡回路出力
-		out << "<before>\t";
-		for (auto i : city)
+		g_OutDebug << "<before>\t";
+		for (auto i : g_City)
 		{
-			out << city.at(i) + 1 << " ";
+			g_OutDebug << g_City.at(i) + 1 << " ";
 		}
-		out << std::endl;
+		g_OutDebug << std::endl;
 
 		twoOptRandom();
 
 		//最適化後に巡回路出力
-		out << "<After>\t";
-		for (auto i : city)
+		g_OutDebug << "<After>\t";
+		for (auto i : g_City)
 		{
-			out << city.at(i) + 1 << " ";
+			g_OutDebug << g_City.at(i) + 1 << " ";
 		}
-		out << std::endl;
+		g_OutDebug << std::endl;
 	}
 
 	if (ENABLE_CHAOS_SEARCH)
 	{
-		out << "EnableChaosSearch" << std::endl;
-		makeFirstTour();
-
-		//最適化前に巡回路出力
-		out << "<before>\t";
-		for (auto i : city)
+		g_OutDebug << "EnableChaosSearch" << std::endl;
+		g_OutCSV << "ATTEMPT_TIMES,COST" << std::endl;
+		for (int k = 0; k < ATTEMPT_TIMES; k++)
 		{
-			out << city.at(i) + 1 << " ";
-		}
-		out << std::endl;
+			makeFirstTour();
 
-		initializeChaosNN();
-
-		for (int t = 1; t < T_TIMES; t++) //tが0回目のときはinitializeChaosNN()で初期化した値とする。tが1回目から0回目の情報を使ってカオスニューラルネットワークの状態を更新していく
-		{
-			for (int i = 0; i < CITY_NUM; i++)
+			//最適化前に巡回路出力
+			g_OutDebug << "<before>\t";
+			for (auto i : g_City)
 			{
-				cnn[t].x[i] = calcX(t, i);
+				g_OutDebug << g_City.at(i) + 1 << " ";
+			}
+			g_OutDebug << std::endl;
 
-				if (cnn[t].x[i] >= 0.5)
+			initializeChaosNN();
+
+			for (int t = 1; t < T_TIMES; t++) //tが0回目のときはinitializeChaosNN()で初期化した値とする。tが1回目から0回目の情報を使ってカオスニューラルネットワークの状態を更新していく
+			{
+				for (int i = 0; i < CITY_NUM; i++)
 				{
-					if (swapPermission(cnn[t].delta_i[i], cnn[t].delta_j[i])) //TODO:エラーをスルーするようなif文。本来いらないはず-> 最大値のij
+					g_Cnn[t].x[i] = calcX(t, i);
+
+					if (g_Cnn[t].x[i] >= 0.5)
 					{
-						twoOptSwap(cnn[t].delta_i[i], cnn[t].delta_j[i]);
-						int distance = calcDistance();
-						out << "debug:After Chaos Search Total Distance:\t" << distance << std::endl;
+						if (swapPermission(g_Cnn[t].delta_i[i], g_Cnn[t].delta_j[i])) //TODO:エラーをスルーするようなif文。本来いらないはず-> 最大値のij
+						{
+							twoOptSwap(g_Cnn[t].delta_i[i], g_Cnn[t].delta_j[i]);
+							g_OutDebug << "debug:After Chaos Search Total Distance:\t" << calcDistance() << std::endl;
+						}
+						else
+						{
+							g_OutDebug << "debug:ERROR main swapPermission false" << std::endl
+									   << "t = " << t
+									   << ", i = " << i << std::endl
+									   << "g_Cnn[t].delta_i[i] = " << g_Cnn[t].delta_i[i] << ", g_Cnn[t].delta_j[i] = " << g_Cnn[t].delta_j[i] << std::endl
+									   << "g_City[g_Cnn[t].delta_i[i]] = " << g_City[g_Cnn[t].delta_i[i]] << ", g_City[g_Cnn[t].delta_j[i]] = " << g_City[g_Cnn[t].delta_j[i]]
+									   << std::endl;
+						}
 					}
 					else
 					{
-						out << "debug:ERROR main swapPermission false" << std::endl
-							<< "t = " << t
-							<< ", i = " << i << std::endl
-							<< "cnn[t].delta_i[i] = " << cnn[t].delta_i[i] << ", cnn[t].delta_j[i] = " << cnn[t].delta_j[i] << std::endl
-							<< "city[cnn[t].delta_i[i]] = " << city[cnn[t].delta_i[i]] << ", city[cnn[t].delta_j[i]] = " << city[cnn[t].delta_j[i]]
-							<< std::endl;
+						continue;
 					}
 				}
-				else
-				{
-					continue;
-				}
+				g_OutDebug << "debug:Remaining t times:\t" << T_TIMES - t - 1 << std::endl;
 			}
-			out << "debug:Remaining t times:\t" << T_TIMES - t - 1 << std::endl;
-		}
 
-		//最適化後に巡回路出力
-		out << "<After>\t";
-		for (auto i : city)
-		{
-			out << city.at(i) + 1 << " ";
+			//最適化後に巡回路出力
+			g_OutDebug << "<After>\t";
+			for (auto i : g_City)
+			{
+				g_OutDebug << g_City.at(i) + 1 << " ";
+			}
+			g_OutDebug << std::endl;
+			g_OutDebug << "debug:After Chaos Search Total Distance:\t" << calcDistance() << std::endl;
+
+			g_OutCSV << k + 1 << "," << calcDistance() << std::endl;
 		}
-		out << std::endl;
-		int distance = calcDistance();
-		out << "debug:After Chaos Search Total Distance:\t" << distance << std::endl;
 	}
 	return 0;
 }
 
-//TSPをedgeへ格納する関数
-void inputTSP(void)
+//TSPをg_Edgeへ格納する関数
+inline void inputTSP(void)
 {
 	int d1, d2, x;
-	if (in.fail())
+	if (g_TSP_In.fail())
 	{
 		std::cerr << "inputTSP:Cannot open file\n";
 		exit(0);
 	}
 	std::string str;
-	while (getline(in, str)) //txtの中身一行ずつループ
+	while (getline(g_TSP_In, str)) //txtの中身一行ずつループ
 	{
 		std::stringstream ss(str);
-		ss >> d1 >> d2 >> x;	  //1行に空白が入る度、入れる変数が変わる
-		edge[d1 - 1][d2 - 1] = x; //TSPを表す2次元vectorへ格納、例) 1 -> 2 のコストを要素へ記録 edge[0][1] = 1から2へ行くためのコスト格納
+		ss >> d1 >> d2 >> x;		//1行に空白が入る度、入れる変数が変わる
+		g_Edge[d1 - 1][d2 - 1] = x; //TSPを表す2次元vectorへ格納、例) 1 -> 2 のコストを要素へ記録 g_Edge[0][1] = 1から2へ行くためのコスト格納
 	}
 
 	//入力されたデータを一旦出力
@@ -187,33 +193,33 @@ void inputTSP(void)
 	// {
 	// 	for (int j = 0; j < CITY_NUM; j++)
 	// 	{
-	// 		out << "data[" << i << "]"
+	// 		g_OutDebug << "data[" << i << "]"
 	// 			<< "[" << j << "]"
 	// 			<< "="
 	// 			<< " "
-	// 			<< edge.at(i).at(j) << std::endl;
+	// 			<< g_Edge.at(i).at(j) << std::endl;
 	// 	}
 	// }
-	// out << std::endl;
+	// g_OutDebug << std::endl;
 }
 
 //初回巡回路をランダムに作成する関数
-void makeFirstTour(void)
+inline void makeFirstTour(void)
 {
 	//巡回路vectorの中身を空にする
-	if (city.empty() == false)
+	if (g_City.empty() == false)
 	{
-		city.clear();
+		g_City.clear();
 	}
 
 	//巡回路vectorに0~citynum-1までの数を順番に入れる
 	for (int i = 0; i < CITY_NUM; i++)
 	{
-		city.push_back(i);
+		g_City.push_back(i);
 	}
 
 	//巡回路シャッフル
-	shuffle(city.begin(), city.end(), engine);
+	shuffle(g_City.begin(), g_City.end(), g_Engine);
 }
 
 //巡回路の総コスト計算関数
@@ -222,9 +228,9 @@ inline int calcDistance(void)
 	int sum = 0;
 	for (int i = 0; i < CITY_NUM - 1; i++)
 	{
-		sum += edge[city[i]][city[i + 1]];
+		sum += g_Edge[g_City[i]][g_City[i + 1]];
 	}
-	sum += edge[city[CITY_NUM - 1]][city[0]];
+	sum += g_Edge[g_City[CITY_NUM - 1]][g_City[0]];
 
 	return sum;
 }
@@ -243,11 +249,11 @@ inline void twoOptRandom(void)
 		//2opt point set
 		do //2optができる2点をランダムに生成
 		{
-			r1 = dist1(engine); //ランダム生成点1
-			r2 = dist2(engine); //ランダム生成点2
+			r1 = dist1(g_Engine); //ランダム生成点1
+			r2 = dist2(g_Engine); //ランダム生成点2
 		} while (!(twoOptPermission(r1, r2)));
 
-		std::vector<int> oldCity = city; //最短ルート保存用vector、2optの前後で合計のコストと比較し2opt後でコストが増えればこの変数を利用し、ロールバックする
+		std::vector<int> oldCity = g_City; //最短ルート保存用vector、2optの前後で合計のコストと比較し2opt後でコストが増えればこの変数を利用し、ロールバックする
 
 		int distance1 = calcDistance(); //合計距離比較用変数1
 
@@ -259,12 +265,12 @@ inline void twoOptRandom(void)
 		//2opt前後で合計距離比較。大きければロールバック
 		if (distance1 < distance2)
 		{
-			city = oldCity; //ロールバック
+			g_City = oldCity; //ロールバック
 		}
 		else //小さいまたは同じコストの場合、現在のコストを出力し、残り2opt回数を減らす
 		{
-			out << "debug:After 2opt Total Distance:\t" << distance2 << std::endl
-				<< "debug:Remaining 2opt times:\t" << TWO_OPT_TIMES - times + 1 << std::endl;
+			g_OutDebug << "debug:After 2opt Total Distance:\t" << distance2 << std::endl
+					   << "debug:Remaining 2opt times:\t" << TWO_OPT_TIMES - times + 1 << std::endl;
 
 			times--;
 		}
@@ -293,8 +299,8 @@ inline bool twoOptPermission(int p1, int p2)
 	{
 		if (p2 == CITY_NUM - 1)
 		{
-			range1 = edge[p1][p1 + 1] + edge[p2][0]; //繋ぎ変える前
-			range2 = edge[p1][p2] + edge[p1 + 1][0]; //繋ぎ変えた後
+			range1 = g_Edge[p1][p1 + 1] + g_Edge[p2][0]; //繋ぎ変える前
+			range2 = g_Edge[p1][p2] + g_Edge[p1 + 1][0]; //繋ぎ変えた後
 			if (range1 >= range2)
 			{
 				return true;
@@ -306,8 +312,8 @@ inline bool twoOptPermission(int p1, int p2)
 		}
 		else
 		{
-			range1 = edge[p1][p1 + 1] + edge[p2][p2 + 1]; //繋ぎ変える前
-			range2 = edge[p1][p2] + edge[p1 + 1][p2 + 1]; //繋ぎ変えた後
+			range1 = g_Edge[p1][p1 + 1] + g_Edge[p2][p2 + 1]; //繋ぎ変える前
+			range2 = g_Edge[p1][p2] + g_Edge[p1 + 1][p2 + 1]; //繋ぎ変えた後
 			if (range1 >= range2)
 			{
 				return true;
@@ -356,7 +362,7 @@ inline void twoOptSwap(int p1, int p2)
 
 		while (swapP1 < swapP2)
 		{
-			std::swap(city[swapP1++], city[swapP2--]);
+			std::swap(g_City[swapP1++], g_City[swapP2--]);
 		}
 	}
 	else
@@ -375,24 +381,24 @@ inline double calcZai(int t, int i)
 {
 	double max = 0.0;
 	bool isFirst = true; //初期max代入時に利用
-	int cityIndex_i = 0; //都市番号とニューロン番号を一致させるため、for文でcity[]の中身とiが一致->都市番号iとニューロン番号iが一致した時のcity[]の要素番号を格納(もっと効率良い方法ありそう)
+	int cityIndex_i = 0; //都市番号とニューロン番号を一致させるため、for文でg_City[]の中身とiが一致->都市番号iとニューロン番号iが一致した時のg_City[]の要素番号を格納(もっと効率良い方法ありそう)
 	int cityIndex_j = 0;
 	for (int k = 0; k < CITY_NUM; k++)
 	{
-		if (city[k] == i)
+		if (g_City[k] == i)
 		{
 			cityIndex_i = k;
 			break;
 		}
 	}
 
-	cnn[t].delta_i[i] = cityIndex_i;
+	g_Cnn[t].delta_i[i] = cityIndex_i;
 
 	for (int j = 0; j < CITY_NUM; j++)
 	{
 		for (int k = 0; k < CITY_NUM; k++)
 		{
-			if (city[k] == j)
+			if (g_City[k] == j)
 			{
 				cityIndex_j = k;
 				break;
@@ -409,12 +415,12 @@ inline double calcZai(int t, int i)
 			if (isFirst)
 			{
 				max = sumZetaBetaDelta;
-				cnn[t].delta_j[i] = cityIndex_j;
+				g_Cnn[t].delta_j[i] = cityIndex_j;
 				isFirst = false;
-				// out << "debug: calcZai First i = " << i
+				// g_OutDebug << "debug: calcZai First i = " << i
 				// 	<< " j = " << j << std::endl
-				// 	<< "city[cityIndex_i] = " << city[cityIndex_i]
-				// 	<< " city[cityIndex_j] = " << city[cityIndex_j] << std::endl
+				// 	<< "g_City[cityIndex_i] = " << g_City[cityIndex_i]
+				// 	<< " g_City[cityIndex_j] = " << g_City[cityIndex_j] << std::endl
 				// 	<< "cityIndex_i = " << cityIndex_i
 				// 	<< " cityIndex_j = " << cityIndex_j
 				// 	<< std::endl;
@@ -422,11 +428,11 @@ inline double calcZai(int t, int i)
 			else if (max <= sumZetaBetaDelta)
 			{
 				max = sumZetaBetaDelta;
-				cnn[t].delta_j[i] = cityIndex_j;
-				// out << "debug: calcZai i = " << i
+				g_Cnn[t].delta_j[i] = cityIndex_j;
+				// g_OutDebug << "debug: calcZai i = " << i
 				// 	<< " j = " << j << std::endl
-				// 	<< "city[cityIndex_i] = " << city[cityIndex_i]
-				// 	<< " city[cityIndex_j] = " << city[cityIndex_j] << std::endl
+				// 	<< "g_City[cityIndex_i] = " << g_City[cityIndex_i]
+				// 	<< " g_City[cityIndex_j] = " << g_City[cityIndex_j] << std::endl
 				// 	<< "cityIndex_i = " << cityIndex_i
 				// 	<< " cityIndex_j = " << cityIndex_j
 				// 	<< std::endl;
@@ -445,7 +451,7 @@ inline double calcEta(int t, int i)
 	{
 		if (k != i)
 		{
-			sum += cnn[t].x[k];
+			sum += g_Cnn[t].x[k];
 		}
 	}
 	return -WEIGHT * sum + WEIGHT;
@@ -454,21 +460,21 @@ inline double calcEta(int t, int i)
 inline double calcZeta(int t, int i) //Zetaはdを含まない形で実装(7)
 {
 	t = t - 1; //tはt+1から入力されるため、前の時刻tを利用するためにtから1を引いている
-	return (KR * cnn[t].zeta[i]) - (ALPHA * cnn[t].x[i]) + R;
+	return (KR * g_Cnn[t].zeta[i]) - (ALPHA * g_Cnn[t].x[i]) + R;
 }
 
 inline int calcDelta(int i, int j)
 {
 	if (swapPermission(i, j))
 	{
-		std::vector<int> oldCity = city;  //最短ルート保存用vector、2optの前後で合計のコストと比較し2opt後でコストが増えればこの変数を利用し、ロールバックする
-		int oldDistance = 0,			  //i-j間の2-opt前巡回路コスト総計
-			newDistance = 0;			  //i-j間の2-opt後巡回路コスト総計
-		oldDistance = calcDistance();	  //swap前にコストを入れる
-		twoOptSwap(i, j);				  //swap
-		newDistance = calcDistance();	  //swap後のコストを入れる
-		city = oldCity;					  //Δijの計算のたびにロールバック
-		return oldDistance - newDistance; //戻り値は削減されればプラスの値を取る
+		std::vector<int> oldCity = g_City; //最短ルート保存用vector、2optの前後で合計のコストと比較し2opt後でコストが増えればこの変数を利用し、ロールバックする
+		int oldDistance = 0,			   //i-j間の2-opt前巡回路コスト総計
+			newDistance = 0;			   //i-j間の2-opt後巡回路コスト総計
+		oldDistance = calcDistance();	   //swap前にコストを入れる
+		twoOptSwap(i, j);				   //swap
+		newDistance = calcDistance();	   //swap後のコストを入れる
+		g_City = oldCity;				   //Δijの計算のたびにロールバック
+		return oldDistance - newDistance;  //戻り値は削減されればプラスの値を取る
 	}
 	else
 	{
@@ -479,13 +485,13 @@ inline int calcDelta(int i, int j)
 
 inline double calcX(int t, int i)
 {
-	cnn[t].eta[i] = calcEta(t, i);
-	cnn[t].zeta[i] = calcZeta(t, i);
-	cnn[t].zai[i] = calcZai(t, i);
-	return sigmoid(cnn[t].zai[i] + cnn[t].eta[i] + cnn[t].zeta[i]);
+	g_Cnn[t].eta[i] = calcEta(t, i);
+	g_Cnn[t].zeta[i] = calcZeta(t, i);
+	g_Cnn[t].zai[i] = calcZai(t, i);
+	return sigmoid(g_Cnn[t].zai[i] + g_Cnn[t].eta[i] + g_Cnn[t].zeta[i]);
 }
 
-void initializeChaosNN(void)
+inline void initializeChaosNN(void)
 {
 	//xの初期値もその後の計算後も0か1にしかならない -> それでいい、そうなってしまうもの
 	std::uniform_real_distribution<> distr(0.0, 1.0);
@@ -493,10 +499,12 @@ void initializeChaosNN(void)
 	{
 		for (int i = 0; i < CITY_NUM; i++)
 		{
-			cnn[t].zai[i] = distr(engine);
-			cnn[t].eta[i] = distr(engine);
-			cnn[t].zeta[i] = distr(engine);
-			cnn[t].x[i] = sigmoid(cnn[t].zai[i] + cnn[t].eta[i] + cnn[t].zeta[i]);
+			g_Cnn[t].zai[i] = distr(g_Engine);
+			g_Cnn[t].eta[i] = distr(g_Engine);
+			g_Cnn[t].zeta[i] = distr(g_Engine);
+			g_Cnn[t].x[i] = sigmoid(g_Cnn[t].zai[i] + g_Cnn[t].eta[i] + g_Cnn[t].zeta[i]);
+			g_Cnn[t].delta_i[i] = 0;
+			g_Cnn[t].delta_j[i] = 0;
 		}
 	}
 }
